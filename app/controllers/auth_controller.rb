025dcd78da1e2ea20aa9e33536f3fcd632f0bb4d
hs_okita.kunio <okita.kunio@heartsoft.co.jp>
2021-08-12 09:15:42 +0900

    session管理を追加。

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,15 @@
 class AuthController < ApplicationController
+  skip_before_action :set_user
+
   def callback
     data = request.env['omniauth.auth']
-    render json: data.to_json
+    save_in_session data
+
+    redirect_to root_url
+  end
+
+  def signout
+    reset_session
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
+  get 'auth/signout'
   # Add route for OmniAuth callback
   match '/auth/:provider/callback', :to => 'auth#callback', :via => [:get, :post]
 end
