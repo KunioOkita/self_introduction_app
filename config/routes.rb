206dd36bcc8f713b307b56c03665872fbf1f6fef
KunioOkita <chocokunio@gmail.com>
2021-08-18 09:36:19 +0900

    部管理画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :departments
   resources :user_self_introductions, only: %i[new create show edit update]
 
   root 'top#index'
   get 'auth/signout'
+  get 'master_data/index'
   # Add route for OmniAuth callback
   match '/auth/:provider/callback', :to => 'auth#callback', :via => [:get, :post]
 end
