715579917718d956b8dfead4cbdb57c965642420
KunioOkita <chocokunio@gmail.com>
2021-08-18 21:59:05 +0900

    skillを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_123637) do
+ActiveRecord::Schema.define(version: 2021_08_18_125033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,14 @@ ActiveRecord::Schema.define(version: 2021_08_18_123637) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.bigint "skill_category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["skill_category_id"], name: "index_skills_on_skill_category_id"
+  end
+
   create_table "user_self_introductions", force: :cascade do |t|
     t.string "birth_place"
     t.string "hobby"
@@ -95,5 +103,6 @@ ActiveRecord::Schema.define(version: 2021_08_18_123637) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "divisions", "departments"
+  add_foreign_key "skills", "skill_categories"
   add_foreign_key "user_self_introductions", "users"
 end
---
+++ spec/factories/skills.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :skill do
+    name { "MyString" }
+    skill_category { nil }
+  end
+end
