7ab4911cd7d3abcc62acba69f24ba8b51c10390a
KunioOkita <chocokunio@gmail.com>
2021-09-10 14:08:54 +0900

    ADアプリ上方をcredentialsに保存

--- config/initializers/omniauth_graph.rb
+++ config/initializers/omniauth_graph.rb
@@ -2,7 +2,7 @@ require "microsoft_graph_auth"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :microsoft_graph_auth,
-           ENV["AZURE_APP_ID"],
-           ENV["AZURE_APP_SECRET"],
-           scope: ENV["AZURE_SCOPES"]
+           ENV["AZURE_APP_ID"].present? ? ENV["AZURE_APP_ID"] : Rails.application.credentials.ad[:app_id],
+           ENV["AZURE_APP_SECRET"].present? ? ENV["AZURE_APP_SECRET"] : Rails.application.credentials.ad[:app_secret],
+           scope: ENV["AZURE_SCOPES"].present? ? ENV["AZURE_SCOPES"] : Rails.application.credentials.ad[:scopes]
 end
