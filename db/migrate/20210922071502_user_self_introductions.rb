95b62c7a9e6e5522e5432e34e48b7c1fd2e2f56e
KunioOkita <chocokunio@gmail.com>
2021-09-22 18:26:36 +0900

    削除した誕生日をもとに戻す

--- db/migrate/20210922071502_user_self_introductions.rb
+++ db/migrate/20210922071502_user_self_introductions.rb
@@ -1,5 +1,7 @@
 class UserSelfIntroductions < ActiveRecord::Migration[6.1]
   def change
+    add_column :user_self_introductions, :birth_month, :integer
+    add_column :user_self_introductions, :birth_day, :integer
     remove_column :user_self_introductions, :introduction, :string
   end
 end
