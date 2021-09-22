2c62a42acc60050805770db50c3ed693c74f4f55
KunioOkita <chocokunio@gmail.com>
2021-09-22 18:26:11 +0900

    サムネイル作成

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module SelfIntroductionApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_storage.variant_processor = :vips
   end
 end
