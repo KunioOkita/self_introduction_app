d60346771f81e9495d01f9b91018866b6999cb16
KunioOkita <chocokunio@gmail.com>
2021-09-25 13:18:42 +0900

    Bugfix: 取得する対象は自己紹介テーブルIDに変更

--- app/controllers/user_divisions_controller.rb
+++ app/controllers/user_divisions_controller.rb
@@ -2,9 +2,7 @@
 
 class UserDivisionsController < ApplicationController
   def update
-    puts "user_division_params => #{user_division_params}"
-    user_division = UserDivision.find_or_initialize_by(id: user_division_params[:division_id])
-    puts "user_division => #{user_division}"
+    user_division = UserDivision.find_or_initialize_by(user_self_introduction_id: user_division_params[:id])
     user_division.update(
       user_self_introduction_id: user_division_params[:id],
       division_id: user_division_params[:division_id]
